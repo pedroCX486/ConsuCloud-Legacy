fb755cf572de4911a10b0cb37d6327eb71c2db30
gafanhotz <pedrogafanhoto@gmail.com>
2017-06-13 14:33:19 +0000

    Adicionar busca no campo de cadastro de consultas

--- consultas/cadastrarconsultas.php
+++ consultas/cadastrarconsultas.php
@@ -43,24 +43,64 @@ if($_SESSION["isMedico"] == true){
       <div class="jumbotron">
         <h1><small>Cadastrar Consultas</small></h1><br>
         <div class="cadastro">
+          
+          <div class="buscar">
+            <form method="get" action="cadastrarconsultas.php">
+            
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon1">Nome do Paciente:</span>
+              <input type="text" class="form-control" name="nomePaciente" aria-describedby="basic-addon1" maxlength="150" placeholder="Campo opcional. Preencha um ou ambos campos." value="<?php echo $_GET['nomePaciente']; ?>">
+            </div>
+            
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon1">RG do Paciente:</span>
+              <input type="text" class="form-control" name="rgPaciente" aria-describedby="basic-addon1" maxlength="150" placeholder="Campo opcional. Preencha um ou ambos campos." value="<?php echo $_GET['rgPaciente']; ?>">
+            </div>
+            
+            <center><button type="submit" class="btn btn-raised btn-info">Buscar Paciente</button> &nbsp; <a href="cadastrarconsultas.php"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></center>
+            
+            </form>
+          </div>
 
           <form method="post" action="cadastrar.php">
-		  
+            
             <div class="form-group">
-              <select required name="paciente" class="form-control">
-              <option disabled selected value="">Nome do Paciente*</option>
-              <?php
-              $select = $mysqli->query("SELECT * FROM pacientes");
-                $row = $select->num_rows;
-                if($row){              
-                  while($get = $select->fetch_array()){
-                  ?>
-                    <option value="<?php echo $get['idPaciente']; ?>" ><?php echo $get['RG'] . " - " . $get['nomePaciente']; ?></option>
-                  <?php
-                  }
-                }
-              ?>
-            </select>
+              <select name="idPaciente" class="form-control">
+                  <option disabled selected value="">Nome do Paciente*</option>
+                    <?php        
+                    if(!empty($_GET['nomePaciente'] || !$_GET['rgPaciente'])){
+                      
+                        $idUsuario = $_SESSION['idUsuario'];
+                  
+                        if($_GET['nomePaciente'] != ""){
+                          $busca = $_GET['nomePaciente'];
+                          
+                          $select = $mysqli->query("SELECT * FROM pacientes WHERE nomePaciente = '$busca'");
+                          
+                        }elseif($_GET['rgPaciente'] != ""){
+                          $busca = $_GET['rgPaciente'];
+                          
+                          $select = $mysqli->query("SELECT * FROM pacientes WHERE RG = '$busca'");
+                          
+                        }else{
+                          $busca1 = $_GET['rgPaciente'];
+                          $busca2 = $_GET['nomePaciente'];
+                          
+                          $select = $mysqli->query("SELECT * FROM pacientes WHERE nomePaciente = '$busca1' AND RG = '$busca2'");
+                          
+                        }
+                    
+                      }
+                      $row = $select->num_rows;
+                      if($row){              
+                        while($get = $select->fetch_array()){
+                          ?>
+                            <option value="<?php echo $get['idPaciente']; ?>" ><?php echo $get['RG'] . ' - ' . $get['nomePaciente']; ?></option>
+                          <?php
+                        }
+                      }
+                    ?>
+                </select>
             </div>
 
             <div class="row">
