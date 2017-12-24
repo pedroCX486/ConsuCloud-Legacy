f157dd0084e93f050b7af88f48c228e3fed71c31
pedrogafanhoto <pedrogafanhoto@gmail.com>
2017-12-24 10:32:36 -0500

    Version bump

--- sobre.php
+++ sobre.php
@@ -59,7 +59,7 @@ if(empty($_SESSION)){
       <br>
 
       <br>
-      <br> Copyright © 2018 - Todos os direitos reservados - Versão 20171224
+      <br> Copyright © 2018 - Todos os direitos reservados - Versão 20171225
 
     </div>
   </div>
