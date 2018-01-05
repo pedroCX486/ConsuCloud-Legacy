de186e19b430062c78a65e97830b03dc685d7b15
Pedro Carneiro <pedrogafanhoto@gmail.com>
2018-01-05 01:36:55 -0300

    Cleanup da barra

--- componentes/barra.php
+++ componentes/barra.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-default navbar-static-top">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a target="navegador" class="navbar-brand" href="../dashboards/dashboard.php">
+      <a class="navbar-brand" href="../dashboards/dashboard.php">
         <img alt="ConsuCloud" src="../assets/brand.png">
       </a>
     </div>
@@ -11,7 +11,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li>
-          <a target="navegador" href="../dashboards/dashboard.php">Início
+          <a href="../dashboards/dashboard.php">Início
             <span class="sr-only">(current)</span>
           </a>
         </li>
@@ -24,11 +24,11 @@
             <!-- Filtrando Menu de Cadastros -->
             <?php
               if($_SESSION["isAdmin"] == true || $_SESSION["isSecretaria"] == true){
-                echo '<li><a target="navegador" href="../pacientes/pacientes.php">Pacientes</a></li>';
-                echo '<li><a target="navegador" href="../consultas/consultas.php">Consultas</a></li>';
+                echo '<li><a href="../pacientes/pacientes.php">Pacientes</a></li>';
+                echo '<li><a href="../consultas/consultas.php">Consultas</a></li>';
               }elseif($_SESSION["isMedico"] == true){
-                echo '<li><a target="navegador" href="../prontuarios/prontuarios.php">Prontuários</a></li>';
-                echo '<li><a target="navegador" href="../exames/exames.php">Exames</a></li>';
+                echo '<li><a href="../prontuarios/prontuarios.php">Prontuários</a></li>';
+                echo '<li><a href="../exames/exames.php">Exames</a></li>';
               }
             ?>
 
@@ -44,8 +44,8 @@
                   <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu">
-                   <li><a target="navegador" href="../receituario/receitas.php">Gerenciamento de Receitas</a></li>
-                    <li><a target="navegador" href="../receituario/receitamanual.php">Receita Manual</a></li>
+                   <li><a href="../receituario/receitas.php">Gerenciamento de Receitas</a></li>
+                    <li><a href="../receituario/receitamanual.php">Receita Manual</a></li>
                 </ul>
               </li>
             ';
@@ -63,34 +63,34 @@
             <!-- Filtrando Menu de Usuário/Configurações -->
             <?php
                 if($_SESSION["isAdmin"] == true){
-                  echo'<li><a target="navegador" href="../usuarios/usuarios.php">Usuários</a></li>';
-                  echo'<li><a target="navegador" href="../planos/planos.php">Planos de Saúde</a></li>';
-                  echo'<li><a target="navegador" href="../config/config.php">Configurações</a></li>';
-                  echo'<li><a target="navegador" href="../logs/logs.php">Logs</a></li>';
-                  echo'<li><a target="navegador" href="../backup/gerenciar_backup.php">Backup</a></li>';
+                  echo'<li><a href="../usuarios/usuarios.php">Usuários</a></li>';
+                  echo'<li><a href="../planos/planos.php">Planos de Saúde</a></li>';
+                  echo'<li><a href="../config/config.php">Configurações</a></li>';
+                  echo'<li><a href="../logs/logs.php">Logs</a></li>';
+                  echo'<li><a href="../backup/gerenciar_backup.php">Backup</a></li>';
                   echo'<li role="separator" class="divider"></li>';
                 }
               ?>
               <li>
-                <a target="navegador" href="../ajuda.php">Ajuda</a>
+                <a href="../ajuda.php">Ajuda</a>
               </li>
               <li>
-                <a target="navegador" href="../reportar.php">Reportar um Erro</a>
+                <a href="../reportar.php">Reportar um Erro</a>
               </li>
               <li>
-                <a target="navegador" href="../sobre.php">Sobre o ConsuCloud</a>
+                <a href="../sobre.php">Sobre o ConsuCloud</a>
               </li>
 
               <!-- Filtrando Agenda e Histórico de Consulta -->
               <?php
                 if($_SESSION["isMedico"] == true){
                   echo '<li role="separator" class="divider"></li>';
-                  echo '<li><a target="navegador" href="../agenda.php">Agenda</a></li>';
-                  echo '<li><a target="navegador" href="../historico/historico_medico.php">Histórico de Consultas</a></li>';
+                  echo '<li><a href="../agenda.php">Agenda</a></li>';
+                  echo '<li><a href="../historico/historico_medico.php">Histórico de Consultas</a></li>';
                 }elseif($_SESSION["isSecretaria"] == true || $_SESSION["isAdmin"] == true){
                   echo '<li role="separator" class="divider"></li>';
-                  echo '<li><a target="navegador" href="../historico/historico_secretaria.php">Histórico de Consultas</a></li>';
-                  echo '<li><a target="navegador" href="../relatorio/relatorio.php">Relatório de Consultas</a></li>';
+                  echo '<li><a href="../historico/historico_secretaria.php">Histórico de Consultas</a></li>';
+                  echo '<li><a href="../relatorio/relatorio.php">Relatório de Consultas</a></li>';
                 }
               ?>
               <li role="separator" class="divider"></li>
