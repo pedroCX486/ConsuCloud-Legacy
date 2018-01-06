8fa6d9bb2adf5415bcaf5c8d1339d64bee941d09
Pedro Carneiro <pedrogafanhoto@gmail.com>
2018-01-06 03:37:55 -0300

    Version bump

--- sobre.php
+++ sobre.php
@@ -61,7 +61,7 @@ if(empty($_SESSION)){
       <br>
 
       <br>
-      <br> Copyright © 2018 - Todos os direitos reservados - Versão 20180106-01BCCB45
+      <br> Copyright © 2018 - Todos os direitos reservados - Versão 20180106-6459D0EE
 
     </div>
   </div>
