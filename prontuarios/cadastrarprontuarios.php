fee09538717a5d2dc1fdde3ddd8a396a94bbdc1c
pedrogafanhoto <pedrogafanhoto@gmail.com>
2018-01-09 00:30:23 -0500

    Oops

--- prontuarios/cadastrarprontuarios.php
+++ prontuarios/cadastrarprontuarios.php
@@ -34,7 +34,7 @@ require("../componentes/db/connect.php");
     <div class="jumbotron">
       <h1>
         <small>Cadastrar Prontuário</small>
-        <a class="anchor" href="prontuarios.php">
+        <a href="prontuarios.php">
           <button class="btn btn-raised btn-danger pull-right" onClick="return confirm('Tem certeza que deseja sair?')">CANCELAR CADASTRO</button>
         </a>
       </h1>
