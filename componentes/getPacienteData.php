0ac835a13f9f894434e2d8fdada536b933807fb9
Pedro Carneiro <44985060+pedroCX486@users.noreply.github.com>
2019-04-06 14:48:18 -0300

    Update getPacienteData.php

--- componentes/getPacienteData.php
+++ componentes/getPacienteData.php
@@ -14,6 +14,7 @@ if($row){
   while($get = $select->fetch_array()){
     $nomePaciente = $get['nomePaciente'];
     $RG = $get['RG'];
+    $CPF = $get['CPF'];
     $RGUFEXP = $get['RGUFEXP'];
     $dataNasc = $get['dataNasc'];
     $telCel = $get['telCel'];
@@ -27,11 +28,12 @@ if($row){
     $endereco_cidade = $get['endereco_cidade'];
     $endereco_cep = $get['endereco_cep'];
     $endereco_estado = $get['endereco_estado'];
+    $notas = $get['notas'];
   }
 }
 
 $dataNasc = date("d/m/Y", strtotime($dataNasc));
 
-echo "RG: " . $RG . "-" . $RGUFEXP . "<br>Data de Nascimento: " . $dataNasc . "<br>Profissão: " . $profissao . "<br>Endereço: " . $endereco_logradouro . " " . $endereco_numero . " " . $endereco_complemento . " - " . $endereco_bairro . " - " .  $endereco_cidade  . " - " . $endereco_estado;
+echo "RG: " . $RG . "-" . $RGUFEXP . "<br>CPF: " . $CPF . "<br>Data de Nascimento: " . $dataNasc . "<br>Profissão: " . $profissao . "<br>Endereço: " . $endereco_logradouro . " " . $endereco_numero . " " . $endereco_complemento . " - " . $endereco_bairro . " - " .  $endereco_cidade  . " - " . $endereco_estado . "<br>Notas: " . $notas;
 
 exit();
